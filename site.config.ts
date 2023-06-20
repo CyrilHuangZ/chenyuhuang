dc5891c6e14022f1457ac396bb3d278954372e5e
Huang Chenyu <125185181+CyrilHuangZ@users.noreply.github.com>
2023-06-20 12:13:13 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'e1a33fcc089c460da33f6367009810a8',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Chenyu Huang',
+  domain: 'https://chenyuhuang.notion.site/e1a33fcc089c460da33f6367009810a8',
+  author: 'Chenyu Huang',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
